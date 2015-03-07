19a47f9ae0cf5a72fc31d27656ebf64855da6341
mdzimmer <matt.dan.zim@gmail.com>
2015-03-07 15:10:40 -0800

    scaling change attempts

--- src/js/states/boot.js
+++ src/js/states/boot.js
@@ -11,8 +11,10 @@ Boot.prototype = {
   create: function () {
     this.game.input.maxPointers = 1;
 
+    /*
     if (this.game.device.desktop) {
-      this.game.stage.scale.pageAlignHorizontally = true;
+      //this.game.stage.scale.pageAlignHorizontally = true;
+      this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
     } else {
       this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
       this.game.scale.minWidth =  480;
@@ -23,6 +25,14 @@ Boot.prototype = {
       this.game.scale.pageAlignHorizontally = true;
       this.game.scale.setScreenSize(true);
     }
+    */
+    var width = 800;
+    var height = 800;
+    this.game.width = width;
+    this.game.height = height;
+    // this.game.stage.bounds.width = width;
+    // this.game.stage.bounds.height = height;
+    // this.game.renderer.resize(width, height);
 
     this.game.state.start('Preloader');
   }
